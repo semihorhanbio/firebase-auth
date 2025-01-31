aa42be882f5259ecaa1fcb1612ebe0383987cb10
Semih Orhan <sorhan3.14@gmail.com>
2025-01-31 23:50:21 +0300

    show payment cards on settings page

---
+++ client/src/components/global/billing/index.tsx
@@ -0,0 +1,12 @@
+import PaymentCard from "./payment-card";
+
+const Billing = () => {
+  return (
+    <div className='flex lg:flex-row flex-col gap-5 w-full lg:w-10/12 xl:w-8/12 container'>
+      <PaymentCard current={"PRO"} label='PRO' />
+      <PaymentCard current={"FREE"} label='FREE' />
+    </div>
+  );
+};
+
+export default Billing;
